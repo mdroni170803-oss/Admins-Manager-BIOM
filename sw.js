68aae37f6b2ce8bb339560aed7cc3aa85b775425
mdroni170803-oss <mdroni170803@gmail.com>
2026-02-27 14:02:52 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,33 @@
-const CACHE_NAME = 'Admin-BIOM-v1';
-const urlsToCache = [
+const CACHE_NAME = 'biom-admin-v1';
+const urlsToCache =[
   './',
   './index.html',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
